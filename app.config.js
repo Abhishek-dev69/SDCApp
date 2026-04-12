19cf06d8d4b180b860089c83fbe3dfc84bade40e
Shlok <punekarshlok@gmail.com>
2026-04-12 14:46:18 +0530

    Added new library for pdf download

--- app.config.js
+++ app.config.js
@@ -2,6 +2,9 @@ export default {
   expo: {
     ...require('./app.json').expo,
     extra: {
+      eas: {
+        projectId: 'e629b11b-6f86-4f4a-83ad-20f5f634c326',
+      },
       apiUrl: process.env.EXPO_PUBLIC_API_URL || 'https://sdcapp-backend-456970553309.asia-south1.run.app'
     }
   }
